acabede98d66d69776914496e40127a9b0fb0da9
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-07-12 18:45:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const boxEL = document.querySelector('.box');
+
+boxEL.addEventListener('click', () => {
+  boxEL.style.backgroundColor = 'blue';
+  boxEL.style.height = '600px';
+  boxEL.style.width = '600px';
+});
